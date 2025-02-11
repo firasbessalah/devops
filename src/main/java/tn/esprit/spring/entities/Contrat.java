42f0d3e3ca24f8cb27de4a4e8109d3fa38257340
firasbessalah <firas.besslah@esprit.tn>
2025-02-11 15:44:43 +0100

    com

--- src/main/java/tn/esprit/spring/entities/Contrat.java
+++ src/main/java/tn/esprit/spring/entities/Contrat.java
@@ -13,30 +13,9 @@ import javax.persistence.TemporalType;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-@Entity
-public class Contrat implements Serializable {
-	
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Long reference;
-	
-	@Temporal(TemporalType.DATE)
-	private Date dateDebut;
-	 
-	private String typeContrat;
- 
-	@JsonIgnore
-	@OneToOne(mappedBy="contrat")
-	private Employe employe;
-
-	private float salaire;
-
+@ssssss
+aadzdazdaz
+azdaz
 	public Contrat() {
 		super();
 	}
